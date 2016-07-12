f70fbb8d86764125d2e6bf5bcca5cacf5e2bd29f
Chris Mague <github@mague.com>
2016-07-11 18:51:49 -0700

    make user configurable

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,33 +1,33 @@
-class vagrant {
+class vagrant ($vagrant_user='vagrant') {
 
-  file { '/home/vagrant/.bash_profile':
+  file { "/home/$vagrant_user/.bash_profile":
     ensure => present,
-    owner  => vagrant,
-    group  => vagrant,
+    owner  => $vagrant_user,
+    group  => $vagrant_user,
     mode   => '0644',
     source => 'puppet:///modules/vagrant/bash_profile'
   }
 
-  file { '/home/vagrant/.bashrc':
+  file { "/home/$vagrant_user/.bashrc":
     ensure => present,
-    owner  => vagrant,
-    group  => vagrant,
+    owner  => $vagrant_user,
+    group  => $vagrant_user,
     mode   => '0644',
     source => 'puppet:///modules/vagrant/bashrc'
   }
 
-  file { '/home/vagrant/.tmux.conf':
+  file { "/home/$vagrant_user/.tmux.conf":
     ensure => present,
-    owner  => vagrant,
-    group  => vagrant,
+    owner  => $vagrant_user,
+    group  => $vagrant_user,
     mode   => '0644',
     source => 'puppet:///modules/vagrant/tmux.conf'
   }
 
-  file { '/home/vagrant/.git-prompt.sh':
+  file { "/home/$vagrant_user/.git-prompt.sh":
     ensure => present,
-    owner  => vagrant,
-    group  => vagrant,
+    owner  => $vagrant_user,
+    group  => $vagrant_user,
     mode   => '0644',
     source => 'puppet:///modules/vagrant/git-prompt.sh'
   }
